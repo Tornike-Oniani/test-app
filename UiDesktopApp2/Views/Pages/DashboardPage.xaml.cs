0c290a472f68a4457f3828ef8b596f12c8bb9a2e
Tornike Oniani <tornike.oniani.1@gmail.com>
2025-02-21 13:30:27 +0400

    removed dragndrop

--- UiDesktopApp2/Views/Pages/DashboardPage.xaml.cs
+++ UiDesktopApp2/Views/Pages/DashboardPage.xaml.cs
@@ -17,38 +17,5 @@ namespace UiDesktopApp2.Views.Pages
 
             InitializeComponent();
         }
-        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (sender is System.Windows.Controls.ListViewItem)
-            {
-                System.Windows.Controls.ListViewItem draggedItem = sender as System.Windows.Controls.ListViewItem;
-                DragDrop.DoDragDrop(draggedItem, draggedItem.DataContext, DragDropEffects.Move);
-                draggedItem.IsSelected = true;
-            }
-        }
-
-        private void ListViewItem_Drop(object sender, DragEventArgs e)
-        {
-            string droppedData = e.Data.GetData(typeof(string)) as string;
-            string target = ((System.Windows.Controls.ListViewItem)(sender)).DataContext as string;
-
-            int removedIdx = temp.Items.IndexOf(droppedData);
-            int targetIdx = temp.Items.IndexOf(target);
-
-            if (removedIdx < targetIdx)
-            {
-                ViewModel.Temp.Insert(targetIdx + 1, droppedData);
-                ViewModel.Temp.RemoveAt(removedIdx);
-            }
-            else
-            {
-                int remIdx = removedIdx + 1;
-                if (ViewModel.Temp.Count + 1 > remIdx)
-                {
-                    ViewModel.Temp.Insert(targetIdx, droppedData);
-                    ViewModel.Temp.RemoveAt(remIdx);
-                }
-            }
-        }
     }
 }
