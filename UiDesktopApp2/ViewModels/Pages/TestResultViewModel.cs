c8fa93722faaf0ebd1aa683de081ff0d35faba10
Tornike Oniani <tornike.oniani.1@gmail.com>
2025-02-28 19:45:38 +0400

    implemented exporting to excel

--- UiDesktopApp2/ViewModels/Pages/TestResultViewModel.cs
+++ UiDesktopApp2/ViewModels/Pages/TestResultViewModel.cs
@@ -13,9 +13,9 @@ namespace UiDesktopApp2.ViewModels.Pages
     {
         #region Observable properties
         [ObservableProperty]
-        private ResultDTO result;
+        private ResultDTO _result;
         [ObservableProperty]
-        private SubjectDTO subject;
+        private SubjectDTO _subject;
         [ObservableProperty]
         private double _averageTimePerKnownImage = 0;
         [ObservableProperty]
@@ -28,6 +28,36 @@ namespace UiDesktopApp2.ViewModels.Pages
             Result = globalState.ResultToBrowse;
             CalculateAvaregaRecognitionTimes();
             Initialize(subjectRepo);
+        }
+        #endregion
+
+        #region Commands
+        [RelayCommand]
+        private void OnCopyResults()
+        {
+            StringBuilder resultBuilder = new StringBuilder(Subject.FullName);
+            AppendCell(resultBuilder, Result.Test.Name);
+            AppendCell(resultBuilder, Result.Date.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            AppendRow(resultBuilder, "");
+            AppendRow(resultBuilder, $"\"Average recognition time\nKnown: {AverageTimePerKnownImage} / Unknown: {AverageTimePerUnkownImage}\"");
+            AppendCell(resultBuilder, $"\"Images Tested\n{Result.ImageSetTimes.Count}\"");
+            AppendCell(resultBuilder, $"\"Max time allowed\n{Result.AvailableTimePerImageVariant}s\"");
+            AppendRow(resultBuilder, "");
+            AppendRow(resultBuilder, "IMAGE");
+            AppendCell(resultBuilder, "RECOGNITION TIME");
+            AppendCell(resultBuilder, "IS UNKNOWN?");
+
+            for (int i = 0; i < Result.ImageSetTimes.Count; i++)
+            {
+                string imageName = Result.ImageSetTimes[i].ImageSet.Name;
+                double imageRecognitionTime = Math.Round(Result.ImageSetTimes[i].Seconds, 2);
+                string isUnknown = Result.ImageSetTimes[i].ImageSet.IsUnknown ? "Yes" : "No";
+                AppendRow(resultBuilder, imageName);
+                AppendCell(resultBuilder, imageRecognitionTime.ToString());
+                AppendCell(resultBuilder, isUnknown);
+            }
+
+            Clipboard.SetText(resultBuilder.ToString());
         }
         #endregion
 
@@ -50,6 +80,14 @@ namespace UiDesktopApp2.ViewModels.Pages
                 Result.ImageSetTimes.Count(ist => ist.ImageSet.IsUnknown),
                 2
                 );
+        }
+        private void AppendCell(StringBuilder builder, string cell)
+        {
+            builder.Append($"\t{cell}");
+        }
+        private void AppendRow(StringBuilder builder, string row)
+        {
+            builder.Append($"\r\n{row}");
         }
         #endregion
     }
